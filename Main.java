d98d31502f855d81d71c3e0c459eea707f338df2
sandg9896 <sandg9896@wrdsb.ca>
2020-10-08 15:07:38 +0000

    Largest Number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * find the largest value inside of that array
+ * @ Gavin 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // create array with 10 spots
+    int[] integers = new int[10];
+
+    // ask user to enter ten numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    // put integers into array using loop
+    for(int i = 0; i < integers.length; i++){
+     integers[i] = input.nextInt();
+    }
+    
+    // create for loop to determine greatest number
+    for(int i = 0; i < integers.length; i++){
+      if(integers[i] > integers.length)
+      System.out.println("The largest number is " + integers[i]);
+    } 
     
   }
 }
